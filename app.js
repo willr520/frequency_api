1817d111b26f1d55074fbcbebebfe07ce7297fc3
willr520 <willr520@newschool.edu>
2023-12-06 13:27:59 -0500

    note-frequencies

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.get("/randomSound", (request, response) => {
 
     response.json({ soundData });
 });
+
 function generateFrequencies(inputString) {
     const frequencyMapping = {
         'A': 277.18 + " Hz",
@@ -71,3 +72,53 @@ function generateFrequencies(inputString) {
 
     return frequencies;
 }
+
+app.get("/note-frequency", (request, response) => {
+    const inputNote = request.query.inputNote || "";
+    const noteFrequency = getNoteFrequency(inputNote);
+
+    if (noteFrequency) {
+        response.json({ noteFrequency });
+    } else {
+        response.status(400).json({ error: "Invalid note input" });
+    }
+});
+
+function getNoteFrequency(note) {
+
+    const noteMapping = {
+        'C': 261.63,
+        'C#': 277.18,
+        'D': 293.66,
+        'D#': 311.13,
+        'E': 329.63,
+        'F': 349.23,
+        'F#': 369.99,
+        'G': 392.00,
+        'G#': 415.30,
+        'A': 440.00,
+        'A#': 466.16,
+        'B': 493.88,
+    };
+
+    const match = note.match(/^([A-Ga-g#]+)([0-9]+)$/);
+
+    if (match) {
+        const noteName = match[1].toUpperCase();
+        const octave = parseInt(match[2], 10);
+
+        if (noteMapping[noteName]) {
+            const frequency = noteMapping[noteName] * Math.pow(2, octave - 4);
+            return frequency;
+        }
+    }
+
+    return null;
+}
+
+
+
+
+//curl -X GET "http://localhost:3000/randomSound?inputString=Nathan"
+//curl -X GET "https://frequency-generator.onrender.com/randomSound?inputString=Nathan"
+//curl -X GET "http://frequency-generator.onrender.com/note-frequency?inputNote=C4"
